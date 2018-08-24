72b17a2c0d3bec52cbad9894284e00cf3d3c5ecb
enoch <qyqdream@gmail.com>
2018-08-24 23:41:33 +0800

    创建首页

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let view = new Vue({
+  el:'#list',
+  data:{
+    selected:null,
+    listMenu:[
+      {num:1,content:'书籍列表'},
+      {num:2,content:'浮层开启与关闭'},
+      {num:3,content:'Tab 切换'},
+      {num:4,content:'一个简单的轮播'},
+    ]
+  },
+  methods:{
+  }
+})
+
+
